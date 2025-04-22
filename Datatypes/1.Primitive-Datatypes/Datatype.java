0cc8d2afe0558064834cd91b7b5a05c0754ac14c
shubhammore34 <shubhammore8338@gmail.com>
2025-04-22 18:23:23 +0530

    Create Datatype.java

---
+++ Datatypes/1.Primitive-Datatypes/Datatype.java
@@ -0,0 +1,47 @@
+public class Datatype {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+
+        byte c = 127;
+        byte d = -128;
+
+        char e = 'a';
+        char f = 65535;
+
+        Short g = 32767;
+        Short h = -32768;
+
+        int i = 2147483647;
+        int j = -2147483648;
+
+        long k = 9223372036854775807L;
+        long l = -9223372036854775808L; //-9223372036854775808l (L --> small l is also valid at the end of long value)
+
+        float m = 9.2231145668f; //~6-7 significant decimal digits
+        float n = - 3.14F;
+
+        double o = 9.2222222222222222222222222222D;
+        double p = -9.233D;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+        System.out.println("e = " + e);
+        System.out.println("f = " + f);
+        System.out.println("g = " + g);
+        System.out.println("h = " + h);
+        System.out.println("i = " + i);
+        System.out.println("j = " + j);
+        System.out.println("k = " + k);
+        System.out.println("l = " + l);
+        System.out.println("m = " + m);
+        System.out.println("n = " + n);
+        System.out.println("o = " + o);
+        System.out.println("p = " + p);
+
+
+
+    }
+}
