d853146e33379cbd7a38904a75bbca51ca22a4c2
shubhammore34 <shubhammore8338@gmail.com>
2025-04-22 18:23:47 +0530

    Create DoubleDatatype.java

---
+++ Datatypes/1.Primitive-Datatypes/DoubleDatatype.java
@@ -0,0 +1,21 @@
+public class DoubleDatatype {
+        
+    public static void main(String args[]) {
+        /* Size of double datatype = 64bits  or  8bytes
+            Max Value double datatype = 1.7976931348623157E308
+            Min Value double datatype = 4.9E-324 */
+        
+        double a = 10.5;     
+        double b = -10.5;   
+        double x = 111122l; 
+        
+        double c = 9223372036854775807l;
+        double d = -9223372036854775808l;
+        
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+        System.out.println("x = " + x);
+    }
+}
