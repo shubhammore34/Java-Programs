eceefda58483794869479be68c20fd48ffd1f9b5
shubhammore34 <shubhammore8338@gmail.com>
2025-04-22 18:22:00 +0530

    Create BooleanDatatype.java

---
+++ Datatypes/1.Primitive-Datatypes/BooleanDatatype.java
@@ -0,0 +1,14 @@
+public class BooleanDatatype {
+    
+    public static void main(String args[]) {
+    	//Default value of boolean = false
+    	
+        boolean a = true;     
+        boolean b = false;    
+        
+        // boolean c = 0;  // BooleanDataType.java:12: error: incompatible types: int cannot be converted to boolean
+        
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
