880d2fd77d42db4c17e01cef93e024b7d14410eb
shubhammore34 <shubhammore8338@gmail.com>
2025-05-03 22:09:05 +0530

    Create _01_DoWhileLoopEx1.java

---
+++ Iterative Statements/_02_do_while_loop/_01_DoWhileLoopEx1.java
@@ -0,0 +1,11 @@
+public class _01_DoWhileLoopEx1 {
+    public static void main(String[] args) {
+        int i = 1; 
+    
+        do {
+            System.out.println("Hello");
+            //++i;
+            i++;
+        } while (i <= 5);
+    }
+}
