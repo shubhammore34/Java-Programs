15e1dd0d5a492f93e0b5fa586b7f70e325b3a6ca
shubhammore34 <shubhammore8338@gmail.com>
2025-05-06 20:16:47 +0530

    initial commit

---
+++ Iterative Statements/_02_do_while_loop/_02_DoWhileLoopEx2.java
@@ -0,0 +1,10 @@
+public class _02_DoWhileLoopEx2 {
+    public static void main(String[] args) {
+        int i = 5; 
+    
+        do {
+            System.out.println("Hello" + i );
+            i--;
+        } while (i >= 1);
+    }
+}
