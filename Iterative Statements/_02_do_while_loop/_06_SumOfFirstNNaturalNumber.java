4dcc709447cf7f22775c352d45f586f3df525f16
shubhammore34 <shubhammore8338@gmail.com>
2025-05-07 22:39:08 +0530

    Create _06_SumOfFirstNNaturalNumber.java

---
+++ Iterative Statements/_02_do_while_loop/_06_SumOfFirstNNaturalNumber.java
@@ -0,0 +1,33 @@
+// Program to find the sum of first n natural numbers
+// positive integers such as 1,2,3,...n are known as natural numbers
+
+
+import java.util.Scanner;
+
+public class _06_SumOfFirstNNaturalNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        
+        int sum = 0;
+        
+         // Approach - 1 => Time Complexity = O(n)
+        int i = 1;
+        do {
+            sum += i;
+            //++i;
+            i++;
+        } while (i <= num);
+        System.out.println("Sum = " + sum);
+        
+        
+        
+        // Approach - 2 => Time Complexity = O(1)
+        /*sum = (num*(num+1) / 2);
+    
+        System.out.println("Sum = " + sum);
+        */
+    }
+}
