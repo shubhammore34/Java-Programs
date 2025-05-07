acc6d7bdca9ec26c6e1b6c74c3628612aec93b70
shubhammore34 <shubhammore8338@gmail.com>
2025-05-07 22:39:53 +0530

    Create _01_ForLoopEx1.java

---
+++ Iterative Statements/_03_for_loop/_01_ForLoopEx1.java
@@ -0,0 +1,8 @@
+public class _01_ForLoopEx1 {
+    public static void main(String[] args) {
+    
+        for(int i=1; i<=5; i++) {
+            System.out.println("Hello" + i);
+        }
+    }
+}
