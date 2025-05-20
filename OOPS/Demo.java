389fbe6fc3a6f0d478c0c419b5d033bbfad67963
shubhammore34 <shubhammore8338@gmail.com>
2025-05-20 10:33:00 +0530

    Create Demo.java

---
+++ OOPS/Demo.java
@@ -0,0 +1,15 @@
+class Student {
+    //String name = "Raju";
+    final String name = "Raju";
+
+    int roll_no;
+    String address;
+}
+
+public class Demo {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        //s1.name = "Vaibhav";
+        System.out.println(s1.name);
+    }
+}
