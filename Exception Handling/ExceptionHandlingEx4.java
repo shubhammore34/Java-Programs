b822bbd3d4a5ea23ec365dbb58578e2587c21421
shubhammore34 <shubhammore8338@gmail.com>
2025-06-22 21:45:02 +0530

    Create ExceptionHandlingEx4.java

---
+++ Exception Handling/ExceptionHandlingEx4.java
@@ -0,0 +1,32 @@
+public class ExceptionHandlingEx4 {
+    public static void main(String[] args) {
+        int marks[] = new int[6];
+        marks[0] = 50;
+        marks[1] = 60;
+        marks[2] = 70;
+        marks[3] = 80;
+        marks[4] = 90/2;
+        //marks[5] = 40/0;
+
+        System.out.println(marks[0]); //50
+        System.out.println(marks[1]); //60
+        System.out.println(marks[2]); //70
+        System.out.println(marks[3]); //80
+        System.out.println(marks[4]); //45
+        
+        //marks[5] = 40/0;
+        //System.out.println(marks[5]); //Exception in thread "main" java.lang.ArithmeticException: / by zero at ExceptionHandlingEx4.main(ExceptionHandlingEx4.java:9)
+
+        try {
+            // marks[10] = 97;
+            // System.out.println(marks[5]);
+
+            marks[8] = 98/0;
+            System.out.println(marks[8]);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+    }
+}
